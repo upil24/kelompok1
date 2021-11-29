e011af8068540f0a99ee0ee8219aba962c2fb2ad
upil24 <bikinnamaajaribet@gmail.com>
2021-11-30 00:53:30 +0700

    ubah view index laporan

--- application/views/cetak/index.php
+++ application/views/cetak/index.php
@@ -3,14 +3,14 @@
 
             <!-- Content Row1 -->
             <div class="row">
-                <div class="col-md-4">
+                <div class="col-md-3">
                     <div class="card">
                         <div class="alert bg-primary" role="alert">
                             <h4 class="text-center text-light">Laporan Pasien</h4>
                         </div>
                         <div class="card-body">
                             <form action="<?= base_url('Laporan/laporan_pasien'); ?>" method="post">
-                                <div class="col-md-6 ml-4">
+                                <div class="col-md-6 ml-2">
                                     <input type="submit" class="btn btn-outline-primary form-control ml-5" value="Cetak" formtarget="_blank">
                                 </div>
                             </form>
@@ -18,15 +18,23 @@
                     </div>
                 </div>
 
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <div class="card">
                         <div class="alert bg-primary" role="alert">
                             <h4 class="text-center text-light">Laporan Kunjungan</h4>
                         </div>
                         <div class="card-body">
                             <form action="<?= base_url('Laporan/laporan_kunjungan'); ?>" method="post">
-                                <div class="col-md-6 ml-4">
-                                    <input type="submit" class="btn btn-outline-primary form-control ml-5" value="Cetak" formtarget="_blank">
+                                <div class="row">
+                                    <div class="col-md-4 ">
+                                        <input type="text" class="form-control datepicker" placeholder="Pilih Tgl" name="awal">
+                                    </div>-
+                                    <div class="col-md-4 ">
+                                        <input type="text" class="form-control datepicker" placeholder="Pilih Tgl" name="akhir">
+                                    </div>
+                                    <div class="col-md-3 ">
+                                        <input type="submit" class="btn btn-outline-primary form-control" value="Cetak" formtarget="_blank">
+                                    </div>
                                 </div>
                             </form>
                         </div>
